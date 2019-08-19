2dcf04cee7fc2234eebb45fd465e7833732a5aff
orlowskilp <lukasz@archanan.io>
2019-08-19 10:30:21 +0800

    Added preformatting of singular words
    Added preformatting of singular words

--- formatters/custom_formatter.py
+++ formatters/custom_formatter.py
@@ -1,2 +1,16 @@
 def format(text):
-	return text.replace('**', '*').replace('*\r\n', '*:\r\n').replace('*\n', '*:\r\n').replace('```', '{noformat}')
+	# Remove unnecessary asterisks from bold text
+	text = text.replace('**', '*')
+
+	# Add missing colons to section titles
+	text = text.replace('*\r\n', '*:\r\n')
+	text = text.replace('*\n', '*:\r\n')
+
+	# Set preformatted blocks of code in JIRA dialect
+	text = text.replace('```', '{noformat}')
+
+	# Set preformatting of singular words
+	text = text.replace(' `', ' {{')
+	text = text.replace('`', '}}')
+
+	return text
